b79bad1faf0e749578a007668871bf2b9ce097ef
geovane oliveira <go399567@gmail.com>
2024-05-06 15:38:27 -0300

    Desenvolvimento da parte lógica, incluindo arquivo Json e todo o JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,21 +17,45 @@ function printPrimaryData(profileDataDetails){
   `
 }
 
-function printHardSkills(profileDataDetails){
+function printSkills(profileDataDetails){
      const hardSkills = document.getElementById('hardSkills')
      hardSkills.innerHTML = profileDataDetails.skills.hardSkills.map(skill => `<span class="li_img"><li><img class="hardSkillsImg" src=${skill.logo} alt=${skill.name} /></li></span>`).join('')
-}
 
-function printSoftSkills(profileDataDetails){
      const softSkills = document.getElementById('softSkills')
-     softSkills.innerHTML = profileDataDetails.skills.softSkills.map(skill => `<li><span>${skill}</span></li>`).join('')
+     softSkills.innerHTML = profileDataDetails.skills.softSkills.map(skill => `<li><span>${skill}</span></li>`).join(' ')
+
+     const languages = document.getElementById('languages')
+     languages.innerHTML = profileDataDetails.skills.idiomas.map(idioma => `<li><span>${idioma}</span></li>`).join('')
+
+     const education = document.getElementById('education')
+     education.innerHTML = profileDataDetails.skills.cursos.map(cursos => {  
+      
+      return `
+      <li>
+       <h3 class="school">${cursos.name}</h3>
+                    <p>${cursos.period}</p>
+                    <p>${cursos.school}</p>
+                    <p>${cursos.topics}</p>
+                                      </li>
+      
+      `
+
+     }).join('')
+
+
+     const portfolio = document.getElementById('portfolio')
+     portfolio.innerHTML = profileDataDetails.skills.portfolio.map(project =>
+       `<li class="project">
+       <h3><a href=${project.url} target="_blank">${project.name}</a></h3>
+       <p>${project.description}</p>
+       </li>`).join('')
 
 }
 
 (async () => {
   const profileDataDetails = await fetchData()
   printPrimaryData(profileDataDetails)
-  printHardSkills(profileDataDetails)
-  printSoftSkills(profileDataDetails)
+  printSkills(profileDataDetails)
+  //printSoftSkills(profileDataDetails)
 
 })()
